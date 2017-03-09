71cb6a83c0d751e472f197f62274ee19f3ca9425
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2017-03-09 13:31:11 -0300

    Fix steps number; Improve messages

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 nc='\033[0m' # No Color
 green='\033[0;32m'
 gray='\033[1;30m'
+blue='\033[0;34m'
 
 
 echo "${green}# [1/9] Creating root directory...${nc}"
@@ -45,25 +46,33 @@ echo "${green}# [7/9] Running migrations...${nc}"
 docker-compose exec api ./bin/rake db:migrate
 
 
-echo "${green}# [7/9] Force rebuild...${nc}"
+echo "${green}# [8/9] Force rebuild...${nc}"
 docker-compose up -d --build
 docker-compose ps
 
 
-echo "${green}# [8/9] Setting hosts up...${nc}"
-app_domain=$(cat '.env' | grep APP_DOMAIN | awk -F '=' '{print $2}' | awk -F ':' '{print $1}')
+echo "${green}# [9/9] Setting hosts up...${nc}"
+env_app_domain=$(cat '.env' | grep APP_DOMAIN | awk -F '=' '{print $2}')
+app_domain=$(echo $env_app_domain | awk -F ':' '{print $1}')
 has_app_domain=$(awk '{print $2}' /etc/hosts | grep -v '^$' | grep "^$app_domain$")
 if ! [ $has_app_domain ]; then
-  sudo -- sh -c -e "echo '127.0.0.1\t$app_domain' >> /etc/hosts" && \
-    echo "${gray}Host [$app_domain] successfully added.${nc}"
+  sudo -- sh -c -e "echo '127.0.0.1\t$app_domain' >> /etc/hosts"
+  echo "${gray}Host [$app_domain] successfully added.${nc}"
+else
+  echo "${gray}Host [$app_domain] already exists.${nc}"
 fi
 
-api_domain=$(cat '.env' | grep API_URL | { read x; echo "${x/API_URL=http:\/\//}"; } | awk -F ':' '{print $1}')
+env_api_domain=$(cat '.env' | grep API_URL | awk -F '=' '{print $2}')
+api_domain=$(echo "${env_api_domain/http:\/\//}" | awk -F ':' '{print $1}')
 has_api_domain=$(awk '{print $2}' /etc/hosts | grep -v '^$' | grep "^$api_domain$")
 if ! [ $has_api_domain ]; then
-  sudo -- sh -c -e "echo '127.0.0.1\t$api_domain' >> /etc/hosts" && \
-    echo "${gray}Host [$api_domain] successfully added.${nc}"
+  sudo -- sh -c -e "echo '127.0.0.1\t$api_domain' >> /etc/hosts"
+  echo "${gray}Host [$api_domain] successfully added.${nc}"
+else
+  echo "${gray}Host [$api_domain] already exists.${nc}"
 fi
 
 
 echo "\nEnjoy, it's all ready to start develop (;"
+echo "-- Use ${blue}http://$env_app_domain${nc} to access the APP"
+echo "-- Use ${blue}$env_api_domain${nc} to access the API"
